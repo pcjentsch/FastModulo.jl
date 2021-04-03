c9515c98f82fb8fb7ad7d99cbdef1a2dbc9ab7c1
pjentsch <pjentsch@uwaterloo.ca>
2021-04-03 00:24:34 -0400

    initial commit

--- src/FastModulo.jl
+++ src/FastModulo.jl
@@ -1,5 +1,27 @@
 module FastModulo
+export Mod,fastmod,fastmod!
+struct Mod
+    modulo::UInt32
+    modulo_inv::UInt64
+    function Mod(d::UInt32)
+        cmod::UInt64 = div(typemax(UInt64), d) + 1
+        return new(d,cmod)
+    end 
+    # function Mod(d::Int32)
+    #     pd::UInt32 =
+    #     cmod::UInt64 = div(typemax(UInt64), d) + 1
+    #     return new(d,cmod)
+    # end
+end
+@inline function fastmod(n::UInt32,T::Mod)::UInt32
+    lowbits::UInt64 = T.modulo_inv * n
+    return (convert(UInt128,lowbits) * T.modulo) >> 64
+end
 
-# Write your package code here.
+#not much better than a loop of fastmod
+function fastmod!(n::Array{UInt32,N},T::Mod) where N
+    n .= (convert.(UInt128, T.modulo_inv .* n) .* T.modulo) .>> 64
+end
+
 
 end
